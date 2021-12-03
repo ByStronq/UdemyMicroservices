239f38ab01e077ac9978908a7fd997d1ba2ffb91
CEMAL YILDIRIM <cemalyildirim@aliniyor.com>
2021-12-03 13:13:06 +0300

    - Category service is written and implemented from ICategoryService.
    - FreeCourse.Shared project referenced FreeCourse.Services.Catalog project.
    
    - Added CategoryService as Scoped to DI (Dependency Injection) Container in Program.cs.
    
    - Moved the access declaration for classes under the FreeCourse.Services.Catalog.Dtos namespace from public to internal.
    
    - The wrong name of a property in IDatabaseSettings has been fixed and the DatabaseSettings class has been re-implemented.
    
    - The name of the ResponseDto class under the FreeCourse.Shared.Dtos namespace has been changed to Response.

--- Services/Catalog/FreeCourse.Services.Catalog/Settings/IDatabaseSettings.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Settings/IDatabaseSettings.cs
@@ -1,9 +1,9 @@
 ﻿namespace FreeCourse.Services.Catalog.Settings
 {
-    public interface IDatabaseSettings
+    internal interface IDatabaseSettings
     {
         public string CourseCollectionName { get; set; }
-        public string CatalogCollectionName { get; set; }
+        public string CategoryCollectionName { get; set; }
         public string ConnectionString { get; set; }
         public string DatabaseName { get; set; }
     }
